882a39f966f69f14244413684e0f051d90ac39e7
Yi Yang (using BlueStudio) <ahyangyi@gmail.com>
2013-06-16 13:35:41 -0700

    Update foot.js according to the Rowell modification

--- js/site/foot.js
+++ js/site/foot.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     UI_register_func();
     UI_register_hotkeys();
-    // UI_set_fileupload();
+    UI_set_fileupload();
     UI_init();
-    UI_session_retrieved($.cookie(bbs_session_cookie));
+    UI_retrieve_session();
 });
