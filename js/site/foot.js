b9ad1d5e6c4363715326d6d95f8fb88138a4c4f6
Yi Yang @ Minthe <ahyangyi@gmail.com>
2014-06-11 23:33:08 -0700

    Fix foot.js

--- js/site/foot.js
+++ js/site/foot.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-    UI_register_func();
-    UI_register_hotkeys();
-    UI_set_fileupload();
     UI_init();
-    UI_retrieve_session();
+    retrieve_session();
 });
